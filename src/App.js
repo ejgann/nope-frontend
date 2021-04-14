e3393ac964bc4b7db8cfdcdf0448ac14568f931d
Emily Gann <aspentree79@gmail.com>
2021-04-14 13:35:41 -0400

    fixed issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/reviews', {
+      method: 'GET'
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+
   render() {
     return (
       <div className="App">
-        APP 
-        </div>
+        <h1>APP</h1> 
+      </div>
     );
   }
 }
