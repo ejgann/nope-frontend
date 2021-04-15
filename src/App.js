ab2bf4e6649ecad4cf1cf12e1a00d506f19f61a1
Emily Gann <aspentree79@gmail.com>
2021-04-15 14:28:32 -0400

    installed packages and imported packages to index.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/reviews', {
-      method: 'GET'
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-  }
+  // TO TEST RAILS API DATA COMING IN OKAY
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/restaurants', {
+  //     method: 'GET'
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
 
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+
+// will need to set up our store
 
 
 ReactDOM.render(
