0f5cdf39f0dc15aa7979d6ebafc441bd44fef0b9
Emily Gann <aspentree79@gmail.com>
2021-04-15 17:01:05 -0400

    set up index.js file and created store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-// will need to set up our store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+    //  an action is sent to reducer and deciding, based on how it's set up, what we want to update about current store. reducer returns new version of the store.
 
 
 ReactDOM.render(
-  <App />, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // We wrap components we want to have access to the store inside Provider tags
+
+  , document.getElementById('root'));
